3a5fb1e85a35c2cf8acac386a01be8b2d11fe7f5
Jetmind <spitsyn.na@phystech.edu>
2026-03-20 17:40:50 +0300

    beautiful build.sh

--- ex1/build.sh
+++ ex1/build.sh
@@ -1,6 +1,13 @@
+MODULE_NAME=$1
+
 python3 -m venv venv
 source venv/bin/activate
 pip install pybind11
 
-cmake .
+mkdir build
+cd build
+
+cmake ..
 cmake --build .
+
+cp "${MODULE_NAME}.cpython-312-x86_64-linux-gnu.so" "../${MODULE_NAME}.so"
