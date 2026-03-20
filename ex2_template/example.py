85fe4a0495b6327567c4daf11268d918ce496461
Nickolay Spitsyn <50750489+jetminded@users.noreply.github.com>
2026-03-20 14:48:33 +0300

    ex2

---
+++ ex2_template/example.py
@@ -0,0 +1,4 @@
+import library as lib
+
+print(lib.myadd(1, 2))
+print(lib.myadd("a", "b"))
---
+++ ex2_template/library.cpp
@@ -0,0 +1,18 @@
+// Include library that allows embedding of pybind
+
+// into the engine
+#include <pybind11>
+#include <string>
+namespace py = pybind11; // shorten what we need to type
+
+template <class T>
+T myadd(T a, T b){
+    return a+b;
+}
+
+PYBIND11_MODULE(library, m){
+    m.doc() = "Templated library with some exposed types";
+    m.def("myadd", &myadd<int>);
+    m.def("myadd", &myadd<double>);
+    m.def("myadd", &myadd<std::string>);
+}
